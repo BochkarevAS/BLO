9d5419e5b8930bbf593d91a0d12644554565f679
BochkarevAS <bochkarev-vl@mail.ru>
2018-07-18 10:25:40 +1000

    Commit

---
+++ src/Entity/Security/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entity\Security;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user", schema="security")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
