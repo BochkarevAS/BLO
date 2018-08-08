1f129553b2ba0ed91ea832de5cc5f43508eae6e4
BochkarevAS <bochkarev-vl@mail.ru>
2018-08-08 16:31:43 +1000

    Commit

---
+++ src/Security/Encoder/PasswordEncoder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Security\Encoder;
+
+use App\Entity\Auth\User;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Security\Core\Encoder\BasePasswordEncoder;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+
+class PasswordEncoder extends BasePasswordEncoder
+{
+    private $container;
+
+    private $user;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function encodePassword($raw, $salt)
+    {
+        if ($this->isPasswordTooLong($raw)) {
+            throw new BadCredentialsException('Invalid password.');
+        }
+
+        if ($this->user && $this->user->getSalt()) {
+            return hash('sha1', $raw);
+        }
+
+        return md5($this->user->getEmail() . md5($raw));
+    }
+
+    public function isPasswordValid($encoded, $raw, $salt)
+    {
+        /**
+         * Этот костыль нужен для того что бы старые пользователи могли входить в новую систему.
+         * Раньше шифрование пароля делалось следующим образом md5($email . md5(password));
+         */
+        $this->user = $this->container->get('doctrine')->getRepository(User::class)->findBy(['password' => $encoded])[0];
+
+        return $this->comparePasswords($encoded, $this->encodePassword($raw, $salt));
+    }
+}
