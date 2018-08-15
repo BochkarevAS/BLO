c2b9fb206e00f0969f15ef1a1aa80a8b37fbbb4f
BochkarevAS <snake-vl@mail.ru>
2018-08-15 14:59:22 +1000

    Commit

--- src/Repository/Tyres/TyresRepository.php
+++ src/Repository/Tyres/TyresRepository.php
@@ -9,23 +9,27 @@ class TyresRepository extends EntityRepository
 {
     public function search(Tyre $tyre)
     {
-        $brand       = $tyre->getBrand();
-        $models      = $tyre->getModel();
-        $thorn       = $tyre->getThorn();
-        $seasonality = $tyre->getSeasonality();
-        $quantity    = $tyre->getQuantity();
-        $diameter    = $tyre->getDiameter();
-        $width       = $tyre->getWidth();
-        $height      = $tyre->getHeight();
-        $city        = $tyre->getCity();
-        $company     = $tyre->getCompany();
+        $brand        = $tyre->getBrand();
+        $models       = $tyre->getModel();
+        $thorn        = $tyre->getThorn();
+        $seasonality  = $tyre->getSeasonality();
+        $quantity     = $tyre->getQuantity();
+        $diameter     = $tyre->getDiameter();
+        $width        = $tyre->getWidth();
+        $height       = $tyre->getHeight();
+        $availability = $tyre->getAvailability();
+        $condition    = $tyre->getCondition();
+        $city         = $tyre->getCity();
+        $company      = $tyre->getCompany();
 
         $qb = $this->createQueryBuilder('t')
-            ->addSelect('b, m, s, thorn')
+            ->addSelect('b, m, s, a, c, thorn')
             ->leftJoin('t.brand', 'b')
             ->leftJoin('t.model', 'm')
             ->leftJoin('t.seasonality', 's')
             ->leftJoin('t.thorn', 'thorn')
+            ->leftJoin('t.availability', 'a')
+            ->leftJoin('t.condition', 'c')
             ->leftJoin('t.city', 'city')
             ->leftJoin('t.company', 'company');
 
@@ -67,6 +71,16 @@ class TyresRepository extends EntityRepository
         /* Фильтр по высота профиля (%) */
         if ($height) {
             $qb->andWhere('t.height = :height')->setParameter('height', $height);
+        }
+
+        /* Фильтр по состоянию */
+        if ($availability) {
+            $qb->andWhere('t.availability = :availability')->setParameter('availability', $availability);
+        }
+
+        /* Фильтр по наличию */
+        if ($condition) {
+            $qb->andWhere('t.condition = :condition')->setParameter('condition', $condition);
         }
 
         /* Фильтр по городам */
