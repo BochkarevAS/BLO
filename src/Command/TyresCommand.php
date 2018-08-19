26f5adb257d036d4f43dd3422b71717c90d96b72
BochkarevAS <bochkarev-vl@mail.ru>
2018-08-19 13:59:59 +1000

    Merge branch 'master' of https://github.com/BochkarevAS/BLO
    # Conflicts:
    #	.env.dist
    #	composer.json
    #	composer.lock
    #	config/packages/doctrine.yaml
    #	config/services.yaml
    #	package.json
    #	src/Command/PartsCommand.php
    #	src/Command/TyresCommand.php
    #	src/Controller/Tyres/TyresController.php
    #	src/Entity/Tyres/Tyre.php
    #	templates/parts/index.html.twig
    #	templates/tyres/_form.html.twig
    #	templates/tyres/index.html.twig

--- src/Command/TyresCommand.php
+++ src/Command/TyresCommand.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Command;
-
+use App\Entity\Client\Availability;
 use App\Entity\Client\Company;
+use App\Entity\Client\Condition;
 use App\Entity\Client\Price;
 use App\Entity\Region\City;
 use App\Entity\Tyres\Brand;
@@ -14,15 +14,11 @@ use Doctrine\ORM\EntityManager;
 use GuzzleHttp\Client;
 use League\Csv\Reader;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-<<<<<<< HEAD
-=======
 use Symfony\Component\Config\Definition\Exception\Exception;
->>>>>>> d64030f00bcd61c2232c1535f463e9fcecdb7685
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
-
 class TyresCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -32,47 +28,27 @@ class TyresCommand extends ContainerAwareCommand
             ->setDescription('Import tyres from CSV file')
         ;
     }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $now = new \DateTime();
         $output->writeln('<comment>Start : ' . $now->format('d-m-Y G:i:s') . ' ---</comment>');
-
         $this->import($input, $output);
-
         $now = new \DateTime();
         $output->writeln('<comment>End : ' . $now->format('d-m-Y G:i:s') . ' ---</comment>');
     }
-
     protected function import(InputInterface $input, OutputInterface $output)
     {
         $file = '616.csv';
-<<<<<<< HEAD
-//        $file = 'test.csv';
-
-        $path = $this->getContainer()->get('kernel')->getProjectDir() . '/public/' . DIRECTORY_SEPARATOR . $file;
-        $em   = $this->getContainer()->get('doctrine')->getManager();
-
-        $prices = $em->getRepository(Price::class)->findAllPriceByCompany(new \DateTime());
-
-        $client = new Client([
-            'base_uri' => 'http://parser.bimbilo.ru/',
-=======
         $file = '795.csv';
-        $file = 'tyres_min.csv';
+//        $file = 'tyres_min.csv';
 //        $file = 'test.csv';
-
         $path = $this->getContainer()->get('kernel')->getProjectDir() . '/public/prices/' . DIRECTORY_SEPARATOR . $file;
         $em   = $this->getContainer()->get('doctrine')->getManager();
-
         $prices = $em->getRepository(Price::class)->findAllPricesByCompany(new \DateTime());
-
         $client = new Client([
             'base_uri' => 'http://p.bimbilo.ru/',
->>>>>>> d64030f00bcd61c2232c1535f463e9fcecdb7685
             'timeout'  => 10
         ]);
-
         $response = $client->request('POST', '/api', [
             'form_params' => [
                 'auth'   => 'bimbilo_13062018',
@@ -81,49 +57,34 @@ class TyresCommand extends ContainerAwareCommand
                 'args'   => [1, 1, $prices]
             ]
         ]);
-
         $contents = $response->getBody()->getContents();
-
-<<<<<<< HEAD
-        if (!$contents) {
-            return;
-=======
         dump($contents);
-
         if (!$contents) {
             throw new Exception('Error');
->>>>>>> d64030f00bcd61c2232c1535f463e9fcecdb7685
         }
-
         $stopwatch = new Stopwatch();
         $stopwatch->start('sanitize');
-
         $reader = Reader::createFromPath($path);
         $reader->setDelimiter(';');
         $reader->setHeaderOffset(0);
         $header = [
             'header', 'tire_type', 'year', 'brand', 'model', 'width_mm',
             'height_proc', 'wheel_width_inc', 'wheel_height_inc', 'diameter_mm',
-            'landing_diameter_inc', 'commercial', 'seasonality', 'status',
+            'landing_diameter_inc', 'commercial', 'seasonality', 'condition',
             'quantity', 'thorn', 'ply_rating', 'index_of_speed', 'load_index',
             'mud_at', 'dirt_mt', 'axis', 'presence_of_camera', 'radius', 'moto_class',
             'in_order', 'tra_code', 'lt_tire_function', 'price', 'availability',
             'description', 'pictures', 'opt', 'region', 'city', 'link_youtube'
         ];
         $records = $reader->getRecords($header);
-
         $i = 0;
         $batchSize = 1000;
         $company = 'Vladmotors';
-
         $progress = new ProgressBar($output, count($reader));
         $progress->start();
-
         foreach ($records as $offset => $record) {
             $hash = md5($record['quantity'] . $record['diameter_mm'] . $record['height_proc'] . $record['width_mm'] . $record['model'] . $record['index_of_speed'] . $record['load_index'] . $record['brand'] . $record['pictures']);
-
             $tyre = $em->getRepository(Tyre::class)->findOneBy(['hash' => $hash]);
-
             if ($tyre === null) {
                 $tyre = new Tyre();
                 $this->insert($tyre, $hash, $record, $em, $company);
@@ -132,31 +93,24 @@ class TyresCommand extends ContainerAwareCommand
                 $this->insert($tyre, $hash, $record, $em, $company);
                 $em->merge($tyre);
             }
-
             if (($i % $batchSize) === 0) {
                 $em->flush();
                 $em->clear();
-
                 $event = $stopwatch->lap('sanitize');
                 $progress->advance($batchSize);
                 $now = new \DateTime();
                 $output->writeln(' of tyres imported ... | ' . $now->format('d-m-Y G:i:s') . ' | memory used : ' . number_format($event->getMemory() / 1048576, 2) . ' MB');
             }
-
             $i++;
         }
-
         $em->flush();
         $em->clear();
-
         $stopwatch->stop('sanitize');
         $progress->finish();
     }
-
     private function insert(Tyre $tyre, $hash, array $record, EntityManager $em, $company)
     {
         $serializer = $this->getContainer()->get('serializer');
-
         $tyre->setHash($hash);
         $tyre->setPrice((int) $record['price']);
         $tyre->setWidth((int) $record['width_mm']);
@@ -164,49 +118,40 @@ class TyresCommand extends ContainerAwareCommand
         $tyre->setQuantity((int) $record['quantity']);
         $tyre->setHeight((int) $record['height_proc']);
         $tyre->setPrice((int) $record['price']);
-<<<<<<< HEAD
-=======
-        $tyre->setAvailability(mb_convert_encoding($record['availability'], 'UTF-8', 'Windows-1251'));
->>>>>>> d64030f00bcd61c2232c1535f463e9fcecdb7685
-
         if ($record['brand']) {
             $brand = $em->getRepository(Brand::class)->findByName(mb_convert_encoding($record['brand'], 'UTF-8', 'Windows-1251'));
             $tyre->setBrand($brand);
         }
-
         if ($record['model']) {
             $model = $em->getRepository(Model::class)->findByName(mb_convert_encoding($record['model'], 'UTF-8', 'Windows-1251'));
             $tyre->setModel($model);
         }
-
         if ($record['seasonality']) {
             $seasonality = $em->getRepository(Seasonality::class)->findByName(mb_convert_encoding($record['seasonality'], 'UTF-8', 'Windows-1251'));
             $tyre->setSeasonality($seasonality);
         }
-
         if ($record['thorn']) {
             $thorn = $em->getRepository(Thorn::class)->findByName(mb_convert_encoding($record['thorn'], 'UTF-8', 'Windows-1251'));
             $tyre->setSeasonality($thorn);
         }
-
+        if ($record['availability']) {
+            $availability = $em->getRepository(Availability::class)->findByName(mb_convert_encoding($record['availability'], 'UTF-8', 'Windows-1251'));
+            $tyre->setAvailability($availability);
+        }
+        if ($record['condition']) {
+            $condition = $em->getRepository(Condition::class)->findByName(mb_convert_encoding($record['condition'], 'UTF-8', 'Windows-1251'));
+            $tyre->setCondition($condition);
+        }
         $city = $em->getRepository(City::class)->findByName(mb_convert_encoding($record['city'], 'UTF-8', 'Windows-1251'));
-
         if ($city) {
             $tyre->setCity($city);
         }
-
         $company = $em->getRepository(Company::class)->findByName($company);
-
         if ($company) {
             $tyre->setCompany($company);
         }
-
-<<<<<<< HEAD
-        $json = $serializer->serialize(['link' => mb_convert_encoding($record['pictures'], 'UTF-8', 'Windows-1251')], 'json');
-=======
         $pictures = explode(',', $record['pictures']);
         $json = $serializer->serialize($pictures, 'json');
->>>>>>> d64030f00bcd61c2232c1535f463e9fcecdb7685
         $tyre->setPicture($json);
     }
 }
